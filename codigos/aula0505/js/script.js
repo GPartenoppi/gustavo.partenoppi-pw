7dc489c62a516d9f425dd1c60c9a1e95eb0137ef
Partenoppi <gustavo.partenoppi@gmail.com>
2026-05-05 08:08:06 -0300

    Add files via upload

---
+++ codigos/aula0505/js/script.js
@@ -0,0 +1,30 @@
+ //Estrutura de repetição While
+var x = 0;
+var soma = 0
+while(x<=5){
+      soma=soma+x
+
+     //incrementador
+     x++
+
+         console.log("Testando repetição"+soma)
+}
+
+//criar um array passando por todos os elemnetos
+var arr = ['teste1','teste2','teste3','a'];
+
+var y=0;
+//passando por todos os elemntos
+while(y<=3){
+     console.log(arr[y]);
+     y++
+}
+
+//passando por todas as palavras
+var palavras='GUSTAVO'
+var i=0
+//passando por todos os elemnetos
+while (i<7){
+     console.log(palavras[i])
+     i++
+}
